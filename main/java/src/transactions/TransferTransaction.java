3d09f4ba328f50dc3af457a8e86bab1151f756c3
Chizuru <chizuru.yamauchi@dci-student.org>
2024-02-17 12:45:33 +0100

    add execute method in TransferTransactioin class

--- main/java/src/transactions/TransferTransaction.java
+++ main/java/src/transactions/TransferTransaction.java
@@ -20,6 +20,19 @@ public class TransferTransaction implements Transaction{
     @Override
     public void execute(){
         // code for transfer transaction
-        System.out.println("Executing Transfer Transaction");
+        // Validate the transfer amount(ensure it is not negative)
+        if (transferAmount <= 0) {
+            System.out.println("Transfer amount is negative. Please enter the positive transfer amount.");
+            return;
+        }
+        // Update the user's account balance
+        double newBalanceForAuthenticatedUser = authenticatedUser.getAccountBalance() - transferAmount;
+        authenticatedUser.setAccountBalance(newBalanceForAuthenticatedUser);
+
+        double newBalanceForRecipientUser = recipientUser.getAccountBalance() + transferAmount;
+        recipientUser.setAccountBalance(newBalanceForRecipientUser);
+
+        // Display a conformation message
+        System.out.println("Transfer successful. The money has sent to " + recipientUser + "\n The amount: " + transferAmount + ".\n" + "Your new balance is: " + newBalanceForAuthenticatedUser);
     }
 }
